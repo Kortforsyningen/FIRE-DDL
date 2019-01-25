16500b98528e0105ff568b2a4bc140384b7c30c6
Thomas Knudsen <busstoptaktik@users.noreply.github.com>
2019-01-25 13:31:23 +0100

    Epsg descriptions (#49)
    * Tilføjet beskrivelser for SRIDTYPE og SRIDNAMESPACE
    
    * Rettet stavefejl DRV90 til DVR90 (forekommer stadig i FIRE_ADM.SRIDTYPE.sql, som skal autogenereres)
    
    * Indfør EPSG-koder hvor de findes, fjern LOC registry (resterende flyttet til DK el EPSG)
    
    * Indfør EPSG-koder hvor de findes, fjern LOC registry (resterende flyttet til DK el EPSG). Nu med alle detaljer gemt

--- Migration/MakeSRIDType.sql
+++ Migration/MakeSRIDType.sql
@@ -1,6 +1,6 @@
 ﻿/* -------------------------------------------------------------------------- */
-/* Make SRIDTYPE data. 
-/* File: MakeSRIDType.sql   
+/* Make SRIDTYPE data.
+/* File: MakeSRIDType.sql
 /* -------------------------------------------------------------------------- */
 
 /*
@@ -10,47 +10,106 @@ ikke tilladte namespaces er anvendt i ca. 10 stk - se nedenfor.
 
 DELETE FROM SRIDNAMESPACE;
 
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','DK');
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','EPSG');
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','FO');
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','GL');
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','LOC');
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','NKG');
-INSERT INTO SRIDNAMESPACE (BESKRIVELSE, NAMESPACE) VALUES ('Ingen beskrivelse','TS');
+INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('DK',
+    'SDFE register over danske systemer')
+INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('EPSG',
+    'IOGP Geodetic Parameter Registry')
+INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('FO',
+    'SDFE register over færøske systemer')
+INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('GL',
+    'SDFE register over grønlandske systemer')
+INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('NKG',
+    'Nordisk Kommission for Geodæsi register over nordisk/baltiske systemer')
+INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('TS',
+    'SDFE register over tidsrækker')
 
 
 DELETE FROM SRIDTYPE;
 
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:DVR90');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4937');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4747');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','FO:FVR09');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4230');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4231');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4258');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4747');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','GL:4747');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','LOC:GI44');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','LOC:GM91');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:GS');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:GSB');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','LOC:HPOT_DRV90');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:KK');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','LOC:KN44');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4326');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','LOC:MSL');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:OS');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:S34J');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:S34S');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:S45B');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','DK:SB');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:4936');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','TS:LRL');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:25832');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:23032');
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID) VALUES ('Ingen beskrivelse','EPSG:23033');
+/* (Pseudo)globale systemer */
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4326',
+    'Geografiske koordinater 2D: WGS84')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4258',
+    'Geografiske koordinater 2D: ETRS89')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4937',
+    'Geografiske koordinater 3D: ETRS89')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4936',
+    'Geocentrisk kartesiske koordinater 3D: ETRS89')
 
-INSERT INTO SRIDTYPE (BESKRIVELSE, SRID)
-SELECT 'Ingen beskrivelse','TS:' || IDENT FROM refadm.REFNR_IDENT@refgeo WHERE IDENT_TYPE = 'jessen';
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4230',
+    'Geografiske koordinater 2D: ED50')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4231',
+    'Geografiske koordinater 2D: ED87')
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:5714',   /* Was: LOC:MSL */
+    'Kotesystem: Lokal middelvandstand')
+
+
+
+/* Danske systemer */
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:5799',
+    'Kotesystem: Dansk Vertikal Reference 1990')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:HPOT_DVR90',
+    'Kotesystem: Geopotentialhøjder DVR90'
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:5733',
+    'Kotesystem: Dansk Normal Nul')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:GI44',
+    'Kotesystem: Dansk Normal Nul, Sjælland')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:GM91',
+    'Kotesystem: Dansk Normal Nul, Jylland')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:KN44',
+    'Kotesystem: Københavns nul 1944')
+
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:25832',
+    'Projicerede koordinater 2D: UTM zone 32, ETRS89')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:23032',
+    'Projicerede koordinater 2D: UTM zone 32, ED50')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:23033',
+    'Projicerede koordinater 2D: UTM zone 33, ED50')
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:GS',
+    'Projicerede koordinater 2D: Generalstabens Konformt Koniske System')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:GSB',
+    'Projicerede koordinater 2D: Generalstabens Konformt Koniske System Bornholm')
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:KK',
+    'Projicerede koordinater 2D: Københavns Kommunes System')
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:OS',
+    'Projicerede koordinater 2D: System Ostenfeld')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:S34J',
+    'Projicerede koordinater 2D: System 34 Jylland')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:S34S',
+    'Projicerede koordinater 2D: System 34 Sjælland')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:S45B',
+    'Projicerede koordinater 2D: System 45 Bornholm')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('DK:SB',
+    'Projicerede koordinater 2D: System Storebælt')
+
+
+/* Færøske systemer */
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:5317',
+    'Kotesystem: Færøsk Vertikal Reference 2009')
+
+
+/* Grønlandske systemer */
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4747',
+    'Geografiske koordinater 2D: GR96')
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('EPSG:4909',
+    'Geografiske koordinater 3D: GR96')
+
+
+/* Tidsrækker */
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE) VALUES ('TS:LRL',
+    'Vandstandsbrætsnettilknytning')
+
+INSERT INTO SRIDTYPE (SRID, BESKRIVELSE)
+SELECT 'TS:' || IDENT FROM refadm.REFNR_IDENT@refgeo WHERE IDENT_TYPE = 'jessen';
 
 COMMIT;
