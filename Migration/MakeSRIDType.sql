be3d9c3dd73913c35c3fd8c69c2ac8505da09d48
Nikolaj Kamstrup (EFKM-SDFE) <X005974@PROD.SITAD.DK>
2019-01-28 07:07:07 +0100

    Various typos corrections

--- Migration/MakeSRIDType.sql
+++ Migration/MakeSRIDType.sql
@@ -6,87 +6,87 @@
 DELETE FROM SRIDNAMESPACE;
 
 INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('DK',
-    'SDFE register over danske systemer')
+    'SDFE register over danske systemer');
 INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('EPSG',
-    'IOGP Geodetic Parameter Registry')
+    'IOGP Geodetic Parameter Registry');
 INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('FO',
-    'SDFE register over færøske systemer')
+    'SDFE register over færøske systemer');
 INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('GL',
-    'SDFE register over grønlandske systemer')
+    'SDFE register over grønlandske systemer');
 INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('NKG',
-    'Nordisk Kommission for Geodæsi register over nordisk/baltiske systemer')
+    'Nordisk Kommission for Geodæsi register over nordisk/baltiske systemer');
 INSERT INTO SRIDNAMESPACE (NAMESPACE, BESKRIVELSE) VALUES ('TS',
-    'SDFE register over tidsrækker')
+    'SDFE register over tidsrækker');
 
 DELETE FROM SRIDTYPE;
 
 /* (Pseudo)globale systemer */
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4326',
-    'Geografiske koordinater 2D: WGS84')
+    'Geografiske koordinater 2D: WGS84');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4258',
-    'Geografiske koordinater 2D: ETRS89')
+    'Geografiske koordinater 2D: ETRS89');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4937',
-    'Geografiske koordinater 3D: ETRS89')
+    'Geografiske koordinater 3D: ETRS89');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4936',
-    'Geocentrisk kartesiske koordinater 3D: ETRS89')
+    'Geocentrisk kartesiske koordinater 3D: ETRS89');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4230',
-    'Geografiske koordinater 2D: ED50')
+    'Geografiske koordinater 2D: ED50');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4231',
-    'Geografiske koordinater 2D: ED87')
+    'Geografiske koordinater 2D: ED87');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:5714',   /* Was: LOC:MSL */
-    'Kotesystem: Lokal middelvandstand')
+    'Kotesystem: Lokal middelvandstand');
 
 /* Danske systemer */
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:5799',
-    'Kotesystem: Dansk Vertikal Reference 1990')
+    'Kotesystem: Dansk Vertikal Reference 1990');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:HPOT_DVR90',
-    'Kotesystem: Geopotentialhøjder DVR90'
+    'Kotesystem: Geopotentialhøjder DVR90');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:5733',
-    'Kotesystem: Dansk Normal Nul')
+    'Kotesystem: Dansk Normal Nul');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:GI44',
-    'Kotesystem: Dansk Normal Nul, Sjælland')
+    'Kotesystem: Dansk Normal Nul, Sjælland');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:GM91',
-    'Kotesystem: Dansk Normal Nul, Jylland')
+    'Kotesystem: Dansk Normal Nul, Jylland');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:KN44',
-    'Kotesystem: Københavns nul 1944')
+    'Kotesystem: Københavns nul 1944');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:25832',
-    'Projicerede koordinater 2D: UTM zone 32, ETRS89')
+    'Projicerede koordinater 2D: UTM zone 32, ETRS89');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:23032',
-    'Projicerede koordinater 2D: UTM zone 32, ED50')
+    'Projicerede koordinater 2D: UTM zone 32, ED50');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:23033',
-    'Projicerede koordinater 2D: UTM zone 33, ED50')
+    'Projicerede koordinater 2D: UTM zone 33, ED50');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:GS',
-    'Projicerede koordinater 2D: Generalstabens Konformt Koniske System')
+    'Projicerede koordinater 2D: Generalstabens Konformt Koniske System');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:GSB',
-    'Projicerede koordinater 2D: Generalstabens Konformt Koniske System Bornholm')
+    'Projicerede koordinater 2D: Generalstabens Konformt Koniske System Bornholm');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:KK',
-    'Projicerede koordinater 2D: Københavns Kommunes System')
+    'Projicerede koordinater 2D: Københavns Kommunes System');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:OS',
-    'Projicerede koordinater 2D: System Ostenfeld')
+    'Projicerede koordinater 2D: System Ostenfeld');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:S34J',
-    'Projicerede koordinater 2D: System 34 Jylland')
+    'Projicerede koordinater 2D: System 34 Jylland');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:S34S',
-    'Projicerede koordinater 2D: System 34 Sjælland')
+    'Projicerede koordinater 2D: System 34 Sjælland');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:S45B',
-    'Projicerede koordinater 2D: System 45 Bornholm')
+    'Projicerede koordinater 2D: System 45 Bornholm');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'DK:SB',
-    'Projicerede koordinater 2D: System Storebælt')
+    'Projicerede koordinater 2D: System Storebælt');
 
 /* Færøske systemer */
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:5317', /* Was: FO:FVR09 */
-    'Kotesystem: Færøsk Vertikal Reference 2009')
+    'Kotesystem: Færøsk Vertikal Reference 2009');
 
 /* Grønlandske systemer */
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4747',
-    'Geografiske koordinater 2D: GR96')
+    'Geografiske koordinater 2D: GR96');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'EPSG:4909',
-    'Geografiske koordinater 3D: GR96')
+    'Geografiske koordinater 3D: GR96');
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'GL:NAD83G',
-    'Geografiske koordinater 2D: NAD83G (DEPRECATED')
+    'Geografiske koordinater 2D: NAD83G (DEPRECATED)');
 
 /* Tidsrækker */
 INSERT INTO SRIDTYPE (SRIDID, SRID, BESKRIVELSE) VALUES (FLOOR(dbms_random.value(1,1E9)), 'TS:LRL',
-    'Vandstandsbrætsnettilknytning')
+    'Vandstandsbrætsnettilknytning');
 
 -- TS:81XXX derived from table ts_dvr90(JNR_BSIDE)
 INSERT INTO SRIDTYPE (BESKRIVELSE, SRID, SRIDID)
@@ -103,6 +103,6 @@ LEFT JOIN refadm.REFNR_IDENT@refgeo landsnr ON ts.REFNR = landsnr.REFNR AND land
 ;
 
 -- Set SRIDID to equal OBJECTID 
-UPDATE SRIDTYPE SET SRIDID = OBEJCTID;
+UPDATE SRIDTYPE SET SRIDID = OBJECTID;
 
 COMMIT;
