8a03d88271231c48663c1023755c6c89f4d4ff21
Kristian Evers <kristianevers@gmail.com>
2019-11-17 11:27:15 +0100

    Fortsæt eksekvering af SQL-scripts selvom der opstår fejl i et enkelt udtryk

--- login.sql
+++ login.sql
@@ -5,7 +5,6 @@ set sqlblanklines on
 set sqlterminator ';'
 show sqlterminator
 show sqlblanklines
-WHENEVER SQLERROR EXIT SQL.SQLCODE
 alter session set current_schema = FIRE_ADM;
 prompt ready login.sql
 set echo on
